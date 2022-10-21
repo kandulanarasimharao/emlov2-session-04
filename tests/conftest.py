c55b82cfe102c74deb91333ad53ea6c09932020f
Narasimha Rao Kandula <kandulanarasimharao@gmail.com>
2022-10-21 04:38:24 +0000

    assign4 final

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,7 +14,7 @@ def cfg_train_global() -> DictConfig:
         with open_dict(cfg):
             cfg.paths.root_dir = str(pyrootutils.find_root())
             cfg.trainer.max_epochs = 1
-            cfg.trainer.limit_train_batches = 0.01
+            cfg.trainer.limit_train_batches = 0.4 #0.01
             cfg.trainer.limit_val_batches = 1.0
             cfg.trainer.limit_test_batches = 0.1
             cfg.trainer.accelerator = "cpu"
